13e21379075a12d6c6faa219ddea1e9412ea38cd
Leonel Bellabarba <leo16_b10@hotmail.com>
2021-12-03 12:05:48 -0300

    Nos comunicamos con Base de datos

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,11 +1,16 @@
 var express = require("express");
 var router = express.Router();
+var novedadesModel = require("../../models/novedadesModels");
 
-
-router.get("/", function(req, res, next){
+router.get("/", async function(req, res, next){
+    
+    var novedades = await novedadesModel.getNovedades();
+    
+    
     res.render("admin/novedades",{
         layout: "admin/layout",
-        usuario: req.session.nombre //este layout es el novedades.hbs
+        usuario: req.session.nombre, //este layout es el novedades.hbs
+        novedades 
     });
 })
 
